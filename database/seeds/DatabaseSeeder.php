2b54753455ce2bf43a76db97a55881db2aacaa88
Diogo Agostinho <2160801@my.ipleiria.pt>
2018-11-27 11:44:58 +0000

    Now for real: Database migrations and seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,13 +5,34 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed the application's database.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(DepartmentsTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+        DB::statement("SET foreign_key_checks=0");
+
+        DB::table('restaurant_tables')->delete();
+        DB::table('users')->delete();
+        DB::table('items')->delete();
+        DB::table('meals')->delete();
+        DB::table('orders')->delete();
+        DB::table('invoices')->delete();
+        DB::table('invoice_items')->delete();
+
+        DB::statement('ALTER TABLE users AUTO_INCREMENT = 0');
+        DB::statement('ALTER TABLE items AUTO_INCREMENT = 0');
+        DB::statement('ALTER TABLE meals AUTO_INCREMENT = 0');
+        DB::statement('ALTER TABLE orders AUTO_INCREMENT = 0');
+        DB::statement('ALTER TABLE invoices AUTO_INCREMENT = 0');
+        DB::statement('ALTER TABLE invoice_items AUTO_INCREMENT = 0');
+        
+        DB::statement("SET foreign_key_checks=1");
+
+        $this->call(RestaurantTablesSeeder::class);
+        $this->call(UsersSeeder::class);
+        $this->call(ItemsSeeder::class);
+        $this->call(MealsSeeder::class);
     }
 }
