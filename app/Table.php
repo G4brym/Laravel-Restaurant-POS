234827538ef7bce5e41b9bc54495a7418e0747d9
Luis Pires <luismpires1997@hotmail.com>
2018-12-03 12:54:57 +0000

    First
    Inicio da implementação

--- app/Table.php
+++ app/Table.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Table extends Model
 {
-    protected $table = 'restaurant_tables';
-
     protected $fillable = [
-        'id',
+        'table_number',
+        'deleted_at',
+        'created_at',
+        'updated_at',
     ];
 
 }
---
+++ resources/js/components/manager.vue
@@ -0,0 +1,27 @@
+<template>
+	<table class="table table-striped">
+		<div class="jumbotron">
+			<h1>Lista mesas</h1>
+		</div>
+	    <thead>
+	        <tr>
+	            <th>Numero da mesa</th>
+	        </tr>
+	    </thead>
+	    <tbody>
+	        <tr v-for="table in this.$store.state.tables">
+                <td>{{ table.table_number }}</td>
+	        </tr>
+	    </tbody>
+	</table>
+</template>
+
+<script type="text/javascript">
+	// Component code (not registered)
+	module.exports={
+	}
+</script>
+
+<style scoped>
+/*	  Specific style applied only on the component*/
+</style>
