b84cc1870e36518d5222b7374562aca6ef048dfe
Luis Pires <luismpires1997@hotmail.com>
2018-12-06 10:35:06 +0000

    Bug fix

--- resources/views/vue/index.blade.php
+++ resources/views/vue/index.blade.php
@@ -146,15 +146,15 @@
 <ul class="sidebar-menu" data-widget="tree">
     <li class="header">MAIN NAVIGATION</li>
     
-    <!-- Waiter-->
-    <li v-if="this.$store.state.user.type === 'waiter'"><router-link to="/waiter"><i class="fa fa-briefcase"></i> <span>Waiter</span></router-link></li>
+    <!-- Waiter -->
+    <li v-if="this.$store.state.user && this.$store.state.user.type === NULL"><router-link to="/waiter"><i class="fa fa-briefcase"></i> <span>Waiter</span></router-link></li>
 
-    <!-- Manager-->
-    <li v-if="this.$store.state.user.type === 'manager'"><router-link to="/dashboard"><i class="fa fa-briefcase"></i> <span>Dashboard</span></router-link></li>
-    <li v-if="this.$store.state.user.type === 'manager'"><router-link to="/table"><i class="fa fa-flag-o"></i> <span>Tables</span></router-link></li>
-    <li v-if="this.$store.state.user.type === 'manager'"><router-link to="/menu"><i class="fa fa-envelope-o"></i> <span>Menu</span></router-link></li>
-    <li v-if="this.$store.state.user.type === 'manager'"><router-link to="/users"><i class="fa fa-users"></i> <span>Users</span></router-link></li>
-    <li v-if="this.$store.state.user.type === 'manager'"><router-link to="/stats"><i class="fa fa-circle"></i> <span>Stats</span></router-link></li>
+    <!-- Manager -->
+    <li v-if="this.$store.state.user && this.$store.state.user.type === 'manager'"><router-link to="/dashboard"><i class="fa fa-briefcase"></i> <span>Dashboard</span></router-link></li>
+    <li v-if="this.$store.state.user && this.$store.state.user.type === 'manager'"><router-link to="/table"><i class="fa fa-flag-o"></i> <span>Tables</span></router-link></li>
+    <li v-if="this.$store.state.user && this.$store.state.user.type === 'manager'"><router-link to="/menu"><i class="fa fa-envelope-o"></i> <span>Menu</span></router-link></li>
+    <li v-if="this.$store.state.user && this.$store.state.user.type === 'manager'"><router-link to="/users"><i class="fa fa-users"></i> <span>Users</span></router-link></li>
+    <li v-if="this.$store.state.user && this.$store.state.user.type === 'manager'"><router-link to="/stats"><i class="fa fa-circle"></i> <span>Stats</span></router-link></li>
     <!--
     <li class="treeview">
         <a href="#">
